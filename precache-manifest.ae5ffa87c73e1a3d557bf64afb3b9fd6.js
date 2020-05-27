a535548ef2ea9e20f5ec3925a1e4d387c62775d5
Axel Campailla <axelcampailla@hotmail.com>
2020-05-27 15:20:43 -0400

    Updates

---
+++ precache-manifest.ae5ffa87c73e1a3d557bf64afb3b9fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8597fec95049ed3e0772cd7a804d103",
+    "url": "/highspot/index.html"
+  },
+  {
+    "revision": "01f36bd2bf654e784412",
+    "url": "/highspot/static/css/main.f294efd3.chunk.css"
+  },
+  {
+    "revision": "8283df093ff77010188f",
+    "url": "/highspot/static/js/2.f0d1cdcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/highspot/static/js/2.f0d1cdcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f36bd2bf654e784412",
+    "url": "/highspot/static/js/main.df9b543e.chunk.js"
+  },
+  {
+    "revision": "3093ab033d7006fca1cd",
+    "url": "/highspot/static/js/runtime-main.bd9f1ece.js"
+  }
+]);
